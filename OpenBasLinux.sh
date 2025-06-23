8032a683ff3a4d2b58166036ef66726793a8119c
AndreiKul-bit <f768661@gmail.com>
2025-06-23 19:06:55 +0300

    Update OpenBasLinux.sh

--- OpenBasLinux.sh
+++ OpenBasLinux.sh
@@ -1,1 +1,8 @@
 curl -s http://10.0.3.10/api/agent/installer/openbas/linux/session-user/135c853c-8ab2-4323-95b8-4258d444201c | sh
+$HOME/.local/openbas-agent-session
+          
+Caldera injector hashes:
+MD5: d604c952bb3c6d96621594d39992c499
+SHA1: 5b6087f87f5f2ae129f888bba799611836eb39a2
+SHA256: 98d1e64445bbef46a36d4724699a386646de78881a1b6f2b346122c76d696c12
+SHA512: ca07dc1d0a5297e29327e483f4f35dadb254d96a16a5c33da5ad048e6965a3863d621518a2be40f1a42226c68cbf5e779382a37ee5baa7dd7c538ec73ce059e8
